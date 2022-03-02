8c091cb3475f8ba6a8a736ee3263d5b4614a029f
brianmwevi <mwevibrian@gmail.com>
2022-03-02 09:00:25 +0300

    script: add akan names form validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,41 @@
+const days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+const akanNames = {
+	male: {
+		Sunday: "Kwasi",
+		Monday: "Kwadwo",
+		Tuesday: "Kwabena",
+		Wednesday: "Kwaku",
+		Thursday: "Yaw",
+		Friday: "Kofi",
+		Saturday: "Kwame",
+	},
+	female: {
+		Sunday: "Akosua",
+		Monday: "Adwoa",
+		Tuesday: "Abenaa",
+		Wednesday: "Akua",
+		Thursday: "Yaa",
+		Friday: "Afua",
+		Saturday: "Ama",
+	},
+};
+
 const getAkan = document.querySelector(".home-akane__btn");
+const akanForm = document.querySelector(".akan-form");
 
 const toggleDisplay = (e) => {
 	const section = e.target.classList[0].split("-")[0];
-    const toHide = document.querySelector(`.${section}`);
+	const toHide = document.querySelector(`.${section}`);
 	const nextSection = toHide.nextElementSibling;
-    console.log(nextSection)
+	console.log(nextSection);
 	toHide.classList.replace("show", "hide");
 	if (nextSection !== "null") {
 		nextSection.classList.replace("hide", "show");
@@ -12,4 +43,18 @@ const toggleDisplay = (e) => {
 		toHide.previousElementSibling.classList.replace("show", "hide");
 	}
 };
+
+const validateDate = (e) => {
+	e.preventDefault();
+	const gender = document.querySelector("input[type=radio]:checked");
+	if (gender === null) return alert("You must select your gender");
+	const userDate = new FormData(akanForm).get("dateOfBirth");
+	const convertedDate = new Date(userDate);
+	const day = convertedDate.getDay();
+	if (day < 0 || day > 7) return alert("Wrong input of day");
+	if (month < 0 || month > 11) return alert("Wrong input of month");
+
+	console.log(akanNames[gender.value][days[day]]);
+};
+akanForm.addEventListener("submit", validateDate);
 getAkan.addEventListener("click", toggleDisplay);
